872ae0130c1448de330f57f8b60752d9c06c5a77
lemercie <lemercie@student.hive.fi>
2024-07-25 18:18:17 +0300

    MVP

--- include/pipex.h
+++ include/pipex.h
@@ -6,12 +6,15 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 15:21:10 by lemercie          #+#    #+#             */
-/*   Updated: 2024/07/24 15:22:51 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/07/25 11:52:51 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+# include <unistd.h> // dup2(), access(), execve()
+# include <wait.h> // waitpid()
+# include <sys/types.h>
 # include "../lib/libft/libft.h"
 #endif
 
--- src/pipex.c
+++ src/pipex.c
@@ -6,22 +6,70 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 15:17:48 by lemercie          #+#    #+#             */
-/*   Updated: 2024/07/24 15:28:18 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/07/25 17:29:27 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
+int	piper(int *pipefd, char **argv, char **envp)
+{
+	int		pid1;
+	int		pid2;
+	char	**cmd1;
+	char	**cmd2;
+	
+	cmd1 = ft_split(argv[1], ' ');
+	cmd2 = ft_split(argv[2], ' ');
+	pid1 = fork();
+	if (pid1 < 0)
+	{
+		ft_printf("Error: fork() failed\n");
+		return (1);
+	}
+	if (pid1 == 0)
+	{
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[0]);
+		close(pipefd[1]);
+		execve(cmd1[0], cmd1, envp);
+	}
+	pid2 = fork();
+	if (pid2 < 0)
+	{
+		ft_printf("Error: fork() failed\n");
+		return (1);
+	}
+	if (pid2 == 0)
+	{
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[0]);
+		close(pipefd[1]);
+		execve(cmd2[0], cmd2, envp);
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	return (0);
+}
+
+
+// return value of fork() is 0 in the child and the actual pid in the parent
+//
+// first execve argument is the path to the binary, second is the arguments 
+// BUT the first argument should also be the path to the binary
 int	main(int argc, char **argv, char **envp)
 {
-	(void) argc;
-	(void) argv;
-	while (*envp)
+	int	pipefd[2];
+		if (argc != 5)
 	{
-		ft_printf("%s\n", *envp);
-		envp++;
+		ft_printf("Error: Wrong number of arguments\n");
+		return (1);
 	}
-
+	if (pipe(pipefd) == -1)
+		return (1);
+	return (piper(pipefd, argv, envp));
 }
 
 

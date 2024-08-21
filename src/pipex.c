5e8769876b9f435fcd9f2768f2713471153d6fa0
lemercie <lemercie@student.hive.fi>
2024-08-21 12:10:14 +0300

    add initiall infile outfile logic

--- src/pipex.c
+++ src/pipex.c
@@ -6,21 +6,31 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 15:17:48 by lemercie          #+#    #+#             */
-/*   Updated: 2024/07/25 17:29:27 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/08/21 12:09:11 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-int	piper(int *pipefd, char **argv, char **envp)
+// return value of fork() is 0 in the child and the actual pid in the parent
+//
+// first execve argument is the path to the binary, second is the arguments 
+// BUT the first argument should also be the path to the binary
+int	piper(char **argv, char **envp)
 {
 	int		pid1;
 	int		pid2;
 	char	**cmd1;
 	char	**cmd2;
+	int		pipefd[2];
 	
-	cmd1 = ft_split(argv[1], ' ');
-	cmd2 = ft_split(argv[2], ' ');
+	if (pipe(pipefd) == -1)
+	{
+		ft_printf("Error: pipe() failed\n");
+		return (1);
+	}
+	cmd1 = ft_split(argv[2], ' ');
+	cmd2 = ft_split(argv[3], ' ');
 	pid1 = fork();
 	if (pid1 < 0)
 	{
@@ -54,22 +64,30 @@ int	piper(int *pipefd, char **argv, char **envp)
 	return (0);
 }
 
-
-// return value of fork() is 0 in the child and the actual pid in the parent
-//
-// first execve argument is the path to the binary, second is the arguments 
-// BUT the first argument should also be the path to the binary
+ // handle file opening before forking
 int	main(int argc, char **argv, char **envp)
 {
-	int	pipefd[2];
-		if (argc != 5)
+	int	infile;
+	int	outfile;
+
+	if (argc != 5)
 	{
 		ft_printf("Error: Wrong number of arguments\n");
 		return (1);
 	}
-	if (pipe(pipefd) == -1)
+	infile = open(argv[1], O_RDONLY);
+	if (infile < 0)
+	{
+		ft_printf("Error: failed to open infile\n");
 		return (1);
-	return (piper(pipefd, argv, envp));
+	}
+	outfile = open(argv[4], O_WRONLY | O_CREAT);
+	if (outfile < 0)
+	{
+		ft_printf("Error: failed to open outfile\n");
+		return (1);
+	}
+	return (piper(argv, envp));
 }
 
 
